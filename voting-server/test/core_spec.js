9374db4a0c1b3a00204e76681fb5a8afb02bdde4
Matt Phillips <matt.phillips@gmail.com>
2016-05-14 16:27:24 -0700

    updated vote() method to expect only the 'vote' partial object;  updated reducer to comply

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -64,47 +64,39 @@ describe('application logic', () => {
 
   describe('vote', () => {
 
-    it('creates a tally for the voted entry', () => {
-      const state = Map({
-        vote: Map({
+    describe('vote', () => {
+
+      it('creates a tally for the voted entry', () => {
+        const state = Map({
           pair: List.of('Trainspotting', '28 Days Later')
-        }),
-        entries: List()
-      });
-      const nextState = vote(state, 'Trainspotting');
-      expect(nextState).to.equal(Map({
-        vote: Map({
+        });
+        const nextState = vote(state, 'Trainspotting')
+        expect(nextState).to.equal(Map({
           pair: List.of('Trainspotting', '28 Days Later'),
           tally: Map({
             'Trainspotting': 1
           })
-        }),
-        entries: List()
-      }));
-    });
+        }));
+      });
 
-    it('adds to existing tally for the voted entry', () => {
-      const state = Map({
-        vote: Map({
+      it('adds to existing tally for the voted entry', () => {
+        const state = Map({
           pair: List.of('Trainspotting', '28 Days Later'),
           tally: Map({
             'Trainspotting': 3,
             '28 Days Later': 2
           })
-        }),
-        entries: List()
-      });
-      const nextState = vote(state, 'Trainspotting');
-      expect(nextState).to.equal(Map({
-        vote: Map({
+        });
+        const nextState = vote(state, 'Trainspotting');
+        expect(nextState).to.equal(Map({
           pair: List.of('Trainspotting', '28 Days Later'),
           tally: Map({
             'Trainspotting': 4,
             '28 Days Later': 2
           })
-        }),
-        entries: List()
-      }));
+        }));
+      });
+
     });
 
   });
